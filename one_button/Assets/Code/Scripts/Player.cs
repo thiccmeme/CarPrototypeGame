6d1f8c9bd340fca5ef6357c27670e0fdfdc9dc31
garanschulz <luigipeg12345@gmail.com>
2024-05-22 22:29:45 -0600

    Added on-hit invincibility
    -When the player gets hit, they are now invincible for a brief period of time.

--- one_button/Assets/Code/Scripts/Player.cs
+++ one_button/Assets/Code/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -10,6 +11,10 @@ public class Player : MonoBehaviour
      [SerializeField]
      private GameOverMenu _gameOverMenu;
 
+     [SerializeField] private float _invulnerabilityTime = 3f;
+
+     private GameObject _car;
+
     public int pHealth;
 
     private Animation anim;
@@ -17,8 +22,7 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         _gameManager = FindObjectOfType<GameManager>();
-        
-
+        _car = GameObject.Find("Car");
     }
     
     private void FixedUpdate()
@@ -37,6 +41,7 @@ public class Player : MonoBehaviour
         if (col.gameObject.tag == "Obstacle")
         {
             pHealth--;
+            StartCoroutine(PainFlash());
             Debug.Log(pHealth);
         }
     }
@@ -45,5 +50,19 @@ public class Player : MonoBehaviour
     {
         
         //_gameManager.Restart();
+    }
+
+    IEnumerator PainFlash()
+    {
+        Color alpha = _car.GetComponent<SpriteRenderer>().color;
+        alpha.a = 0.5f;
+        _car.GetComponent<SpriteRenderer>().color = alpha;
+        _car.GetComponent<BoxCollider2D>().enabled = false;
+        Debug.Log("hit");
+        yield return new WaitForSeconds(_invulnerabilityTime);
+        alpha.a = 1.0f;
+        _car.GetComponent<BoxCollider2D>().enabled = true;
+        _car.GetComponent<SpriteRenderer>().color = alpha;
+        Debug.Log("finished");
     }
 }
